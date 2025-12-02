e8e8866c73dadce8d94b8485d2706d9e185ce5ee
Truong Phu Cuong <45337895+s3713026@users.noreply.github.com>
2025-12-02 12:05:28 +0700

    Add 'sameSite' attribute to cookie configuration

--- src/app.ts
+++ src/app.ts
@@ -62,6 +62,7 @@ app.use(
         cookie: {
             secure: process.env.NODE_ENV === 'production',
             httpOnly: true,
+            sameSite: 'none',      // cross-domain cookie
             maxAge: 24 * 60 * 60 * 1000 // 24 hours
         }
     })
